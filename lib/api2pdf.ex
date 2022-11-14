c404401a34bf2f4062f31d1324cfc45a1799d7b5
Eka Putra <eka@insight.co>
2022-11-14 21:40:36 +0800

    initial commit

---
+++ lib/api2pdf.ex
@@ -0,0 +1,18 @@
+defmodule Api2pdf do
+  @moduledoc """
+  Documentation for `Api2pdf`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Api2pdf.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/api2pdf_test.exs
@@ -0,0 +1,8 @@
+defmodule Api2pdfTest do
+  use ExUnit.Case
+  doctest Api2pdf
+
+  test "greets the world" do
+    assert Api2pdf.hello() == :world
+  end
+end
